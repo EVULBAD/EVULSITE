84008bc67ed52909abd42e9bf25a305fe42a1c47
Dmitry <dmitrydohy@gmail.com>
2023-01-15 21:14:53 -0800

    Optimized and updated javascript
    Mostly what it says on the tin, alongside a handful of tweaks. The cursor-following textbox is fully set up. Doorknob makes a sound. A new overlay for the main render exists. Looking good!

--- main.js
+++ main.js
@@ -1,50 +1,49 @@
-//cursor-following text box.
-let textbox = document.getElementById("textbox");
-const onMouseMove = (e) =>{
-    textbox.style.left = e.pageX + "px";
-    textbox.style.top = e.pageY + "px";
-}
-document.addEventListener("mousemove", onMouseMove);
+//ambience.
+/*let ambience = new Audio("sounds/ambience.mp3");
+window.onload = function() {
+    ambience.loop=true;
+    ambience.play();
+};*/
 
 //animation.
 let button = document.querySelector(".button");
+let timer;
 button.addEventListener("click",(e)=>{
     let aniName = e.target.classList[1];
-    let frameCount = document.getElementById(aniName).children.length;
+    let frameCount = (document.getElementById(aniName).children.length)-3;
     let aniFrames = document.getElementById(aniName);
-    let startFrame = aniFrames.firstElementChild.nextElementSibling;
+    let startFrame = aniFrames.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling;
     let nextFrame = startFrame.nextElementSibling;
+    clearTimeout(timer);
     startFrame.classList.add("hidden"); startFrame.classList.remove("current");
     nextFrame.classList.remove("hidden"); nextFrame.classList.add("current");
-    let currentFrame = nextFrame; //currently: 001
-    nextFrame = nextFrame.nextElementSibling; //next: 002
-    setTimeout(function() {
-        currentFrame.classList.add("hidden"); currentFrame.classList.remove("current");
-        nextFrame.classList.remove("hidden"); nextFrame.classList.add("current");
-        currentFrame = nextFrame; //currently: 002
-        nextFrame = nextFrame.nextElementSibling; //next: 003
-    },41.7);
-    setTimeout(function() {
-        currentFrame.classList.add("hidden"); currentFrame.classList.remove("current");
-        nextFrame.classList.remove("hidden"); nextFrame.classList.add("current");
-        currentFrame = nextFrame; //currently: 003
-        nextFrame = nextFrame.nextElementSibling; //next: 004
-    },(41.7*2));
-    setTimeout(function() {
-        currentFrame.classList.add("hidden"); currentFrame.classList.remove("current");
-        nextFrame.classList.remove("hidden"); nextFrame.classList.add("current");
-        currentFrame = nextFrame; //currently: 004
-        nextFrame = nextFrame.nextElementSibling; //next: 005
-    },(41.7*3));
-    setTimeout(function() {
-        currentFrame.classList.add("hidden"); currentFrame.classList.remove("current");
-        nextFrame.classList.remove("hidden"); nextFrame.classList.add("current");
-        currentFrame = nextFrame; //currently: 005
-        nextFrame = nextFrame.nextElementSibling; //next: null
-    },(41.7*4));
+    let currentFrame = nextFrame;
+    nextFrame = nextFrame.nextElementSibling;
+    for (let i=1; i<=(frameCount-1); i++) {
+        setTimeout(function() {
+            currentFrame.classList.add("hidden"); currentFrame.classList.remove("current");
+            nextFrame.classList.remove("hidden"); nextFrame.classList.add("current");
+            currentFrame = nextFrame;
+            nextFrame = nextFrame.nextElementSibling;
+        },(41.7*i));
+    }
     setTimeout(function() {
         currentFrame.classList.add("hidden"); currentFrame.classList.remove("current");
         startFrame.classList.remove("hidden"); startFrame.classList.add("current");
-        currentFrame = nextFrame; //currently: start
-    },(41.7*5));
-})
+        currentFrame = nextFrame;
+    },(41.7*frameCount));
+    //button audio.
+    let audio = new Audio("sounds/" + aniName + ".mp3"); audio.play();
+    //cursor-following text box.
+    function cursorPal(e) {
+        textbox.style.left = e.pageX + "px"; textbox.style.top = e.pageY + "px";
+    }
+    let textbox = document.getElementById("textbox");
+    textbox.style.left = e.pageX + "px"; textbox.style.top = e.pageY + "px";
+    textbox.classList.remove("hidden");
+    document.addEventListener("mousemove", cursorPal);
+    timer = setTimeout(function() {
+        textbox.classList.add("hidden");
+        document.removeEventListener("mousemove",cursorPal);
+    },5000)
+});
