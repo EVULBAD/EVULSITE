1a791b55c18af95f6378009ec1510f3858341aa3
Dmitry <dmitrydohy@gmail.com>
2023-09-06 22:49:02 -0700

    Form page styling complete
    The form is now fully styled and looking swank. Involved adding some js to make the file uploading limit how many files can be uploaded. I'm thinking I should implement a way to limit file size as well... just in case anybody gets any funny ideas. It'll take just a handful more lines of js, but I'm tired right now honk shoooo mimimimi

--- commissions/commissions.js
+++ commissions/commissions.js
@@ -1,23 +1,31 @@
+//js for file uploading on the form page.
+function uploadFile(target) {
+  if (target.files.length > 3) {
+    alert("you many only upload up to 3 files.");
+  } else if (target.files.length <= 1) {
+    document.getElementById("file-count").innerHTML = " " + target.files.length + " file selected.";
+  } else  {
+    document.getElementById("file-count").innerHTML = " " + target.files.length + " files selected.";
+  }
+}
+
 //background size adjuster thanks to perttu on stack overflow.
 //keeps the scrollbar from resizing the bg, because i'm a tightass about stuff like that.
 fixBackgroundSizeCover = function(event) {
+  let bgImageWidth = 1920,
+    bgImageHeight = 1080,
+    bgImageRatio = bgImageWidth / bgImageHeight,
+    windowSizeRatio = window.innerWidth / window.innerHeight;
 
-    var bgImageWidth = 1920,
-      bgImageHeight = 1080,
-      bgImageRatio = bgImageWidth / bgImageHeight,
-      windowSizeRatio = window.innerWidth / window.innerHeight;
-  
-    if (bgImageRatio > windowSizeRatio) {
-      document.getElementById("twod").style.backgroundSize = 'auto 100vh';
-      document.getElementById("threed").style.backgroundSize = 'auto 100vh';
-    } else {
-      document.getElementById("twod").style.backgroundSize = '100vw auto';
-      document.getElementById("threed").style.backgroundSize = '100vw auto';
-    }
-  };
+  if (bgImageRatio > windowSizeRatio) {
+    document.getElementById("twod").style.backgroundSize = 'auto 100vh';
+    document.getElementById("threed").style.backgroundSize = 'auto 100vh';
+  } else {
+    document.getElementById("twod").style.backgroundSize = '100vw auto';
+    document.getElementById("threed").style.backgroundSize = '100vw auto';
+  }
+};
   
 fixBackgroundSizeCover();
 
 window.addEventListener('resize', fixBackgroundSizeCover());
-
-console.log()
